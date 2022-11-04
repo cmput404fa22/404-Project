b63220dcd4dc470b9a2fd93f3a59b41df2523fa3
signed-long <mrl@ualberta.ca>
2022-11-04 02:16:40 -0600

    made admin dashboard nicer

--- social_distribution/app/admin.py
+++ social_distribution/app/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Author)
+
+@admin.action(description='Register selected users')
+def make_published(modeladmin, request, queryset):
+    queryset.update(registered=True)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'UUID', 'registered',)
+    actions = [make_published]
+
+    def UUID(self, obj):
+        return obj.uuid.hex
+
+
+admin.site.register(Author, AuthorAdmin)
