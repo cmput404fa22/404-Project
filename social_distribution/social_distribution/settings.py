43811f5c717f1af68db812994459120cdc0effc4
Jesse Grywacheski <jegrywacheski@gmail.com>
2022-11-21 14:51:17 -0700

    fix gitignore

--- social_distribution/social_distribution/settings.py
+++ social_distribution/social_distribution/settings.py
@@ -19,14 +19,14 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = " " #os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == 'True'
+DEBUG = True #os.environ.get("DEBUG") == 'True'
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
-HOSTNAME = os.environ.get("HOSTNAME")
+HOSTNAME = '8000' #os.environ.get("HOSTNAME")
 LOGIN_URL = '/login/'
 
 # Application definition
