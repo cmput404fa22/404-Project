146f9f71db2be9c40909a31c646ccfcc80da7ee0
signed-long <mrl@ualberta.ca>
2022-11-22 21:51:36 -0700

    import dj-database-url in settings

--- social_distribution/social_distribution/settings.py
+++ social_distribution/social_distribution/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import django_on_heroku
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
