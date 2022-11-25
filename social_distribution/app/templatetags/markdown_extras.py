b452699b1c4a47a567c2390d86260506f0d05324
Jesse Grywacheski <jegrywacheski@gmail.com>
2022-11-25 13:01:41 -0700

    switch to commonmark

--- social_distribution/app/templatetags/markdown_extras.py
+++ social_distribution/app/templatetags/markdown_extras.py
@@ -1,10 +1,16 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-import markdown as md
+import cmarkgfm
+
+############################################################################
+# Python binding to Commonmark provided by cmarkgfm module
+#   Documentation can be found at https://github.com/theacodes/cmarkgfm
+#   License states permission for commercial and private use
+############################################################################
 
 register = template.Library()
 
 @register.filter()
 @stringfilter
-def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+def markdown(content):
+    return cmarkgfm.markdown_to_html(content)
