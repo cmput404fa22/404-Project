0b086e6c2870b46bf95b2a9d23bf464b9239a245
Mark Holmstrom <mholmstr@ualberta.ca>
2022-11-28 18:02:08 -0700

    HTTPS Redirect
    To make the university not hate the website. From https://help.heroku.com/J2R1S4T8/can-heroku-force-an-application-to-use-ssl-tls we do not have a cert for the FQDN but it should just force the *.herokuapp.com one probably.

--- social_distribution/social_distribution/settings.py
+++ social_distribution/social_distribution/settings.py
@@ -136,6 +136,10 @@ SWAGGER_SETTINGS = {
     'DEFAULT_AUTO_SCHEMA_CLASS': 'api.swagger.CustomSwaggerAutoSchema',
 }
 
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 django_on_heroku.settings(locals())
 
 
