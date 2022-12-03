d26ef0ee16f7a938ba3ac9a0b482e5c7cb62b5bf
MarkHolmstrom <mholmstr@ualberta.ca>
2022-12-03 12:01:47 -0700

    Prod only

--- social_distribution/social_distribution/settings.py
+++ social_distribution/social_distribution/settings.py
@@ -136,9 +136,10 @@ SWAGGER_SETTINGS = {
     'DEFAULT_AUTO_SCHEMA_CLASS': 'api.swagger.CustomSwaggerAutoSchema',
 }
 
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# https://help.heroku.com/J2R1S4T8/can-heroku-force-an-application-to-use-ssl-tls
+if (os.environ.get('PROD') == 'True'):
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
 
 django_on_heroku.settings(locals())
 
